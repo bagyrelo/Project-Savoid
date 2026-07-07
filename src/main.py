9cc8318672029a459039579b8d1402a25b89a182
bagyrelo <gabryelgn50@hotmail.com>
2026-07-06 22:28:09 -0300

    2

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ screen_width, screen_height = 800, 600
 init_window(screen_width, screen_height, "Project Savoid")
 set_target_fps(60)
 
-ground = load_texture("Project Savoid/assets/tux.png")
+ground = load_texture("Project-Savoid/assets/tux.png")
 
 my_player = Player(get_screen_width() // 2, get_screen_height() // 2, 7, 7, 100, 5)
 my_enemy = Skeleton(50, 10, 500, 200)
@@ -25,8 +25,10 @@ while not window_should_close():
 
     # Events
 
-    if check_collision_circles((my_player.posx, my_player.posy), 10, (my_enemy.x, my_enemy.y), 10):
-        my_enemy.hit(my_player)
+    if check_collision_circles((my_player.posx, my_player.posy), 15, (my_enemy.posx, my_enemy.posy), 15):
+        my_enemy.update_attack(my_player, get_frame_time())
+    else:
+        my_enemy.attack_timer = 3.0
 
     # Update
 
@@ -39,7 +41,7 @@ while not window_should_close():
     begin_mode_2d(cam)
 
     draw_texture(ground, screen_width//2 - ground.width//2, screen_height//2 - ground.height//2, WHITE)
-    draw_text(TextFormat(b'%i', ffi.cast("int", my_player.health)), 20, 20, 50, BLUE)
+    draw_text(TextFormat(b'%i', ffi.cast("int", my_player.health)), my_player.posx - 300, my_player.posy - 200, 30, BLUE)
 
 
     my_player.draw()
