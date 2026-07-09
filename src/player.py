ce4c04a3229d544fe6bf03fd40d43014357b728d
bagyrelo <gabryelgn50@hotmail.com>
2026-07-08 21:00:51 -0300

    Atualizar o player.py

--- src/player.py
+++ src/player.py
@@ -9,17 +9,11 @@ class Player:
         self.speedy = speedy
         self.health = health
         self.attack = attack
-<<<<<<< HEAD
         self.texture = load_texture("assets/player.png")
 
     def draw(self):
         return draw_texture(self.texture, self.posx - self.texture.width // 2, self.posy - self.texture.height // 2, WHITE)
-=======
 
-    def draw(self):
-        return draw_circle(self.posx, self.posy, 10, BLUE)
->>>>>>> 717cf33d719835ecbf1ac8daea3323620c112c4d
-    
     def update(self):
         if is_key_down(KEY_W):
             self.posy -= self.speedy
